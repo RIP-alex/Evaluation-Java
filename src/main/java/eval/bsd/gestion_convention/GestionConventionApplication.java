ab7a6ec2c0b9d339bc6892349fe32d9eaa4f25b5
RIP-alex <alexandre.rippling@stagiairesmns.fr>
2024-12-12 07:36:46 +0100

    Mise en place de Spring

---
+++ src/main/java/eval/bsd/gestion_convention/GestionConventionApplication.java
@@ -0,0 +1,13 @@
+package eval.bsd.gestion_convention;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionConventionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionConventionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eval/bsd/gestion_convention/GestionConventionApplicationTests.java
@@ -0,0 +1,13 @@
+package eval.bsd.gestion_convention;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionConventionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
